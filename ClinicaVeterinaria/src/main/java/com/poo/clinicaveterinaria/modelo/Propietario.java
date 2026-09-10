0bbec102541cef227150ab19e875f30c0e2d01e3
AndresOlmos09 <aolmosg@unicartagena.edu.co>
2026-09-10 14:09:31 -0500

    Creando Clase Propietario

--- ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Propietario.java
+++ ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Propietario.java
@@ -4,10 +4,59 @@
  */
 package com.poo.clinicaveterinaria.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Andres
  */
 public class Propietario {
+    private String nombre;
+    private String cedula;
+    private String telefono;
+    private List<Mascota> mascotas;
+    
+    //Constructor
+    public Propietario(String nombre, String cedula, String telefono) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.mascotas = new ArrayList<>();
+    }
+    
+    //GET Y SET
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Mascota> getMascotas() {
+        return mascotas;
+    }
+    
+    public void agregarMascota(Mascota mascota) {
+        mascotas.add(mascota);
+    }
     
 }
