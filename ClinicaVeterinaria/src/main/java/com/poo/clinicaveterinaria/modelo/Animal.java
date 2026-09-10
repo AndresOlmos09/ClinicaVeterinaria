f7af557f54f0bf842ea2513d8683f028c35b8dfe
AndresOlmos09 <aolmosg@unicartagena.edu.co>
2026-09-10 13:16:04 -0500

    Clase animal como Abstract y adicione constructores, getts y setts

--- ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Animal.java
+++ ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Animal.java
@@ -8,11 +8,53 @@ package com.poo.clinicaveterinaria.modelo;
  *
  * @author Andres
  */
-public class Animal {
+public abstract class Animal {
     private String nombre;
     private String especie;
     private int edad;
     private int peso;
+
+    //Constructor
+    public Animal(String nombre, String especie, int edad, int peso) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+        this.peso = peso;
+    }
+    
+    //GETS AND SETS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+    
+    public abstract String obtenerDescripcion();
+    
 }
-
-
--- ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Mascota.java
+++ ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Mascota.java
@@ -9,5 +9,39 @@ package com.poo.clinicaveterinaria.modelo;
  * @author Andres
  */
 public class Mascota extends Animal{
+    private String raza;
+    private String propietario;
     
+    
+    //Constructor
+    public Mascota(String raza, String propietario, String nombre, String especie, int edad, int peso) {
+        super(nombre, especie, edad, peso);
+        this.raza = raza;
+        this.propietario = propietario;
+    }
+    
+    //GET Y SETS
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+    
+    
+    
+    @Override
+    public String obtenerDescripcion() {
+        return getNombre() + " (" + getEspecie() + ", " + raza + "), "
+                + getEdad() + " años, " + getPeso() + " kg";
+    }
 }
