a81dbacf3c13374e148b90eda880f8b8a7291bcc
AndresOlmos09 <aolmosg@unicartagena.edu.co>
2026-09-10 19:50:24 -0500

    Creando Clase Veterinario

--- ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Veterinario.java
+++ ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Veterinario.java
@@ -9,5 +9,43 @@ package com.poo.clinicaveterinaria.modelo;
  * @author Andres
  */
 public class Veterinario {
+    private String nombre;
+    private String especialidad;
+    private String numeroLicencia;
     
+    //Constructor 
+    public Veterinario(String nombre, String especialidad, String numeroLicencia){
+        this.nombre=nombre;
+        this.especialidad=especialidad;
+        this.numeroLicencia=numeroLicencia;
+    }
+    
+    //Getters y Settters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getNumeroLicencia() {
+        return numeroLicencia;
+    }
+
+    public void setNumeroLicencia(String numeroLicencia) {
+        this.numeroLicencia = numeroLicencia;
+    }
+    
+    public void recetar(Consulta c, Medicamento m){
+        consulta.agregarMedicamento(c);
+    }
 }
