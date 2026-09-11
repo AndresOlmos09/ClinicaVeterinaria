23280c969622081874ed8a411480dca9f91507af
AndresOlmos09 <aolmosg@unicartagena.edu.co>
2026-09-10 20:02:50 -0500

    Creando Consulta

--- ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Consulta.java
+++ ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/modelo/Consulta.java
@@ -4,10 +4,80 @@
  */
 package com.poo.clinicaveterinaria.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Andres
  */
 public class Consulta {
+    private String fecha;
+    private String motivo;
+    private String diagnostico;
+    private Mascota m;
+    private Veterinario v;
+    private List<Medicamento> medicamentos;
+
+    //Constructor
+    public Consulta(String fecha, String motivo, String diagnostico, Mascota m, Veterinario v) {
+        this.fecha = fecha;
+        this.motivo = motivo;
+        this.diagnostico = diagnostico;
+        this.m = m;
+        this.v = v;
+        this.medicamentos = new ArrayList<>();
+    }
+    
+    //Getters y Setters
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public Mascota getM() {
+        return m;
+    }
+
+    public void setM(Mascota m) {
+        this.m = m;
+    }
+
+    public Veterinario getV() {
+        return v;
+    }
+
+    public void setV(Veterinario v) {
+        this.v = v;
+    }
+    
+    public List<Medicamento> getMedicamentos(){
+        return medicamentos;
+    }
+    
+    public void agregarMedicamento(Medicamento medicamento){
+        medicamentos.add(medicamento);
+    }
+  
     
 }
