9ac1e209b558c71d956ab81a64b861e9617cc11c
AndresOlmos09 <aolmosg@unicartagena.edu.co>
2026-09-11 13:37:29 -0500

    Creando Clase Main

---
+++ ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/controlador/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poo.clinicaveterinaria.controlador;
+
+/**
+ *
+ * @author Andres
+ */
+public class Main {
+    public static void main(String[] args) {
+        ClinicaController controlador = new ClinicaController();
+        controlador.iniciar();
+    }
+}
