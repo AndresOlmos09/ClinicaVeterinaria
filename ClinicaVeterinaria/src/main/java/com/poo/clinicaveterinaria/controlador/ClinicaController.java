2031e33c680141a6572337a099906f6a6bcb375e
AndresOlmos09 <aolmosg@unicartagena.edu.co>
2026-09-10 20:13:47 -0500

    Creando vista y controlador

---
+++ ClinicaVeterinaria/src/main/java/com/poo/clinicaveterinaria/controlador/ClinicaController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.poo.clinicaveterinaria.controlador;
+import com.poo.clinicaveterinaria.modelo.*;
+
+        
+/**
+ *
+ * @author Andres
+ */
+public class ClinicaController {
+
+    
+}
